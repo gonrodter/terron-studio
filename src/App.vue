939e718c8c45c8d5071c99bebbdf2bdfbee04a9f
Gonzalo <rodriguez.terron.gonzalo@gmail.com>
2026-09-12 11:41:55 +0200

    update Sushi Panda showcase assets

--- src/App.vue
+++ src/App.vue
@@ -181,7 +181,7 @@ const imageSizeMap = {
   "reservas-1.png": { width: 1920, height: 1536 },
   "reservas-2.png": { width: 1920, height: 1536 },
   "reservas-3.png": { width: 1920, height: 1536 },
-  "sushi-panda.png": { width: 1920, height: 1536 },
+  "sushi-panda.png": { width: 1920, height: 970 },
   "sushi-panda-project.png": { width: 1920, height: 1203 },
   "sushi-panda-1.png": { width: 1920, height: 1536 },
   "sushi-panda-2.png": { width: 1920, height: 1536 },
@@ -733,12 +733,12 @@ const heroImageSrcset = [
 
 const heroSlides = [
   "aparicio-2.png", "collecta.png", "reservas-1.png", "pecatto-2.png",
-  "vegan-roll.png", "ducati.png", "locura-2.png", "reservas.png",
+  "sushi-panda-4.png", "vegan-roll.png", "ducati.png", "locura-2.png", "reservas.png",
   "platypool-3.png", "pecatto.png", "vegan-roll-1.png", "aparicio.png",
-  "reservas-2.png", "pecatto-3.png", "aparicio-3.png", "vegan-roll-2.png",
+  "sushi-panda.png", "reservas-2.png", "pecatto-3.png", "aparicio-3.png", "vegan-roll-2.png",
   "burntab.png", "aparicio-4.png",
 ];
-const heroZoomShots = new Set(["collecta.png", "burntab.png", "aparicio.png", "ducati.png", "pecatto.png", "reservas.png", "vegan-roll.png"]);
+const heroZoomShots = new Set(["collecta.png", "burntab.png", "aparicio.png", "ducati.png", "pecatto.png", "reservas.png", "vegan-roll.png", "sushi-panda.png"]);
 const heroSlide = ref(0);
 let heroTimer = null;
 
